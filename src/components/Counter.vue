40bc139deb8c82feedb68a79c1569a5a25352b11
AnzeKovac <anze.kovac@outlook.com>
2020-02-02 23:16:57 +0100

    Adds basic v3 structure

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <h1>ACADEMY</h1>
+    <button @click="increment()">Clicks: {{ state.count }} Double: {{ state.double }}</button>
+  </div>
+</template>
+
+<script>
+  import { reactive, computed } from "vue"
+  export default {
+    setup() {
+      const state = reactive({
+        count: 0,
+        double: computed(() => state.count * 2)
+      })
+
+      function increment() {
+        state.count++;
+      }
+
+      return { state, increment }
+    }
+  }
+</script>
+
+<style>
+</style>
