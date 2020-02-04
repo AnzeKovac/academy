ace51c894f98e365a3f308a40b9268968ee0cf06
AnzeKovac <anze.kovac@agilcon.com>
2020-02-04 08:15:57 +0100

    Adds videojs

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,27 +1,58 @@
 <template>
   <div class="page">
-    <h1>ACADEMY</h1>
+    <h1>Test</h1>
     <button @click="increment()">Clicks: {{ state.count }} Double: {{ state.double }}</button>
+
+    <video ref="videoPlayerRef" class="video-js"></video>
+
   </div>
 </template>
 
 <script>
-  import { reactive, computed } from "vue"
-  export default {
-    setup() {
-      const state = reactive({
-        count: 0,
-        double: computed(() => state.count * 2)
-      })
+import { reactive, computed, onMounted, ref } from "vue";
+import videojs from "video.js";
 
-      function increment() {
-        state.count++;
-      }
+export default {
+  /* eslint-disable no-console */
+  setup() {
+    const videoPlayerRef = ref();
+    const h1ref = ref();
+    let player = ref(null);
+    const playerOptions = {
+      autoplay: true,
+      controls: true,
+      sources: [
+        {
+          src:
+            "https://cdn.jsdelivr.net/npm/big-buck-bunny-1080p@0.0.6/video.mp4",
+          type: "video/mp4"
+        }
+      ]
+    };
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2)
+    });
 
-      return { state, increment }
+    function increment() {
+      state.count++;
     }
+
+    onMounted(() => {
+      console.log(h1ref);
+      console.log(videoPlayerRef);
+      player = videojs(
+        videoPlayerRef.value,
+        playerOptions,
+        function onPlayerReady() {}
+      );
+    });
+
+    return { state, increment, videoPlayerRef, player };
   }
+};
 </script>
 
-<style>
+<style scoped>
+@import './../../node_modules/video.js/dist/video-js.css';
 </style>
